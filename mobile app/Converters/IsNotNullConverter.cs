201b1a3695897cf4f93698443f578cf8897057c9
reagan1133 <reagantong@hotmail.com>
2025-12-02 02:26:05 +0800

    u

---
+++ mobile app/Converters/IsNotNullConverter.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+namespace mobile_app.Converters;
+
+public class IsNotNullConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        // Returns TRUE if the value is not null (i.e., we have an image)
+        return value != null;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
